68487f3193ebb63ebcd248d7fa297151e1ff0152
Sichun Wu <s.wu6@students.uu.nl>
2022-12-22 01:50:43 +0100

    'animation_done'

--- Player.gd
+++ Player.gd
@@ -5,6 +5,9 @@ const MAX_SPEED = 280
 const ACCELERATION = 650
 const FRICTION = 650
 
+onready var animation_tree = $AnimationTree
+onready var animation_State = animation_tree.get("parameters/playback")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,8 +16,12 @@ func _process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_State.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_State.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
