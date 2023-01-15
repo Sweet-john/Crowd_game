cedb2b3eae86537b8f8b65b6415cde24dc04cb88
C.Zhu <113450928+D1oStar@users.noreply.github.com>
2023-01-15 21:55:48 +0100

    HUD update

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -1,17 +1,22 @@
 extends CanvasLayer
 
-signal start_game
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var gaming_hud = $GamingHud
+onready var start_menu = $StartMenu
+onready var game_menu = $GameMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_menu.visible = true
+	game_menu.visible = false
+	gaming_hud.visible = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_StartButton_pressed():
+	start_menu.visible = false
+	gaming_hud.visible = true
+	pass # Replace with function body.
