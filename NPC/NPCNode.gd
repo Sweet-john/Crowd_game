b25cc7362de3af5a65b0c5b82969e8b45caf46bb
Lu Xinliang <1209425127@qq.com>
2023-01-21 16:55:03 +0100

    改进导航逻辑，修复生成到海里的问题，优化性能

--- NPC/NPCNode.gd
+++ NPC/NPCNode.gd
@@ -3,10 +3,11 @@ extends Node
 var NPCScene = preload("res://NPC/NonePlayerCharacter.tscn")
 #var NPCInstance = NPCScene.instance()
 var maxNpcNum = 5000
-var npcIncreasePerFrame = 200
+var npcIncreasePerFrame = 50
 var npcCounter = 0
 var freeLocation: Vector2
 var targetLocation: Vector2
+var generateNPC = true
 #onready var generateRandomPoints = get_node("../WorldMap").inPolygonRandomPointGenerator.new(get_node("../WorldMap").getFreeArea())
 
 # Called when the node enters the scene tree for the first time.
@@ -17,13 +18,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if npcCounter < maxNpcNum:
-		for j in range(npcIncreasePerFrame):
-			freeLocation = Vector2(randi() % (18176 + 5120) - 5120, randi() % (14016 + 6592) - 6592)
-			var NPCInstance = NPCScene.instance()
-			NPCInstance.position = freeLocation
-			add_child(NPCInstance)
-		npcCounter = get_node("/root/Level/Camera2D/HUD").agent_n
-	#get_parent().get_node("Camera2D/HUD").agent_n = npcCounter
-	
-	
+	if generateNPC:
+		if npcCounter < maxNpcNum:
+			for j in range(npcIncreasePerFrame):
+				freeLocation = Vector2(randi() % (18176 + 5120) - 5120, randi() % (14016 + 6592) - 6592)
+				var NPCInstance = NPCScene.instance()
+				NPCInstance.position = freeLocation
+				add_child(NPCInstance)
+			npcCounter = get_node("/root/Level/Camera2D/HUD").agent_n
+		else:
+			generateNPC = false
--- NPC/NonePlayerCharacter.gd
+++ NPC/NonePlayerCharacter.gd
@@ -1,92 +1,143 @@
 extends KinematicBody2D
-export onready var velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1))
+export onready var velocity = Vector2(0,0)
 var path : = PoolVector2Array()
-var hit_flag = 0
+var hit_flag = false
+var path_line = Line2D.new()
 var next_target: Vector2
 var ifEvacuation = false
 #var targetLocation = Vector2(randi() % (3968 + 17664) - 3968, randi() % (4544 + 10496) - 4544)
-var targetLocation
+var targetLocation: Vector2
 onready var speed = 200
+onready var hitCount = 0
+var hitCountThreshole = 50
+var targetRadius = 500
+var player_position: Vector2
+var collision: KinematicCollision2D
+var isNpcGone = false
+var eliminateDistance = 100
 
 var safe_zoon_pos : = PoolVector2Array()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in get_node("/root/Level/WorldMap/EXIT").get_children():
-		safe_zoon_pos.push_back(i.position)
-	get_node("/root/Level/Camera2D/HUD").agent_n += 1
 	randomize()
-	velocity = (velocity.normalized()) * speed
-	yield(get_tree(), "idle_frame")
-	targetLocation = safe_zoon_pos[randi()%safe_zoon_pos.size()]
-	path = get_node("/root/Level/WorldMap/Navigation2D").get_simple_path(position, targetLocation)
-	
-	next_target = path[0]
-	path.remove(0)
-	$startEvacuation.wait_time = randf()*1.0+0.1
-	$startEvacuation.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+	for i in get_node("/root/Level/WorldMap/StaticBody2D").get_children():
+		if Geometry.is_point_in_polygon(position,i.polygon):
+			queue_free()
+			isNpcGone = true
+		else:
+			continue
 	
+	var nerestDistance = 1e10
+	for i in get_node("/root/Level/WorldMap/EXIT").get_children():
+		if position.distance_to(i.position) < nerestDistance:
+			targetLocation = i.position
+			nerestDistance = position.distance_to(i.position)
+		else:
+			continue
+	
+	if isNpcGone == false:
+		yield(get_tree(), "idle_frame")
+		path = get_node("/root/Level/WorldMap/Navigation2D").get_simple_path(position, targetLocation)
+		
+		if path.size() == 0:
+			queue_free()
+		else:
+			get_node("/root/Level/Camera2D/HUD").agent_n += 1
+			
+			path_line.width = 5
+			path_line.points = path
+			
+			next_target = path[0]
+			path.remove(0)
+			$startEvacuation.wait_time = randf()*(nerestDistance/100)+5.0
+			$Timer.wait_time = randf()*1.0+0.1
+			$startEvacuation.start()
+	
+
+func _process(delta):
+	collision = move_and_collide(velocity * delta)
+
+
+func _physics_process(delta):
 	if ifEvacuation:
-		var collision = move_and_collide(velocity * delta)
 		if collision:
-			velocity = velocity.bounce(collision.normal)
-			hit_flag = 1
+			velocity = Vector2(0,0)
+			hit_flag = true
+			hitCount += 1
 			$Timer.start()
-			if collision.collider.has_method("hit"):
-				collision.collider.hit()
-		if position.distance_to(next_target) < 100 and hit_flag == 0:
+
+		if position.distance_to(targetLocation) < targetRadius:
+			get_node("/root/Level/Camera2D/HUD").agent_n -= 1
+			get_node("/root/Level/Camera2D/HUD").agent_safe_n += 1
+			queue_free()
+		
+		if hitCount > hitCountThreshole and hit_flag == true:
+			print("replanning for " + str($Zombie))
+			path = get_node("/root/Level/WorldMap/Navigation2D").get_simple_path(position, targetLocation)
+			path_line.points = path
+			next_target = path[0]
+			path.remove(0)
+			hit_flag = false
+			hitCount = 0
+			if position.distance_to(next_target) < 100:
+				if path.size() != 0:
+					next_target = path[0]
+					velocity = next_target - position
+					velocity = (velocity.normalized()) * speed
+					path.remove(0)
+					hit_flag = false
+					$Timer.stop()
+				else:
+					get_node("/root/Level/Camera2D/HUD").agent_safe_n += 1
+					get_node("/root/Level/Camera2D/HUD").agent_n -= 1
+					queue_free()
+			else:
+				velocity = next_target - position
+				velocity = (velocity.normalized()) * speed
+				hit_flag = false
+				$Timer.stop()
+			
+		if position.distance_to(next_target) < 100 and hit_flag == false:
 			if path.size() != 0:
 				next_target = path[0]
 				velocity = path[0] - position
 				velocity = (velocity.normalized()) * speed
 				path.remove(0)
-			else:
-				velocity = Vector2(0,0)
 
-		var player_position = get_node("/root/Level/MainCharacter").position
-		#print(position.distance_to(player_position))
-		if position.distance_to(player_position) < 192:
-			get_node("/root/Level/Camera2D/HUD").score += 1
-			queue_free()
-		elif position.distance_to(targetLocation) < 512:
-			get_node("/root/Level/Camera2D/HUD").agent_safe_n += 1
-			queue_free()
-	else:
-		var collision = move_and_collide(velocity * delta)
-		if collision:
-			velocity = velocity.bounce(collision.normal)
-			if collision.collider.has_method("hit"):
-				collision.collider.hit()
+	player_position = get_node("/root/Level/MainCharacter").position
+	if position.distance_to(player_position) < eliminateDistance:
+		get_node("/root/Level/Camera2D/HUD").score += 1
+		get_node("/root/Level/Camera2D/HUD").agent_n -= 1
+		queue_free()
+
 
 func _exit_tree():
-	get_node("/root/Level/Camera2D/HUD").agent_n -= 1
+	#get_node("/root/Level/Camera2D/HUD").agent_n -= 1
+	pass
+
 
 func _on_Timer_timeout():
-	path = get_node("/root/Level/WorldMap/Navigation2D").get_simple_path(position, targetLocation)
-	next_target = path[0]
-	path.remove(0)
 	if position.distance_to(next_target) < 100:
 		if path.size() != 0:
 			next_target = path[0]
 			velocity = next_target - position
 			velocity = (velocity.normalized()) * speed
 			path.remove(0)
-			hit_flag = 0
+			hit_flag = false
 			$Timer.stop()
 		else:
 			velocity = Vector2(0,0)
-			hit_flag = 0
+			hit_flag = false
 			$Timer.stop()
 	else:
 		velocity = next_target - position
 		velocity = (velocity.normalized()) * speed
-		hit_flag = 0
+		hit_flag = false
 		$Timer.stop()
 
 
 func _on_startEvacuation_timeout():
 	ifEvacuation = true
-	$startEvacuation.queue_free()
+	$startEvacuation.stop()
